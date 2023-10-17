5b543b825461b2a37cd3a6c20ac5ee146d3e44a6
WeeScottishPuffin <supposedbrit@gmail.com>
2023-10-17 11:55:42 +0000

    Eh forgor to commit 💀

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import flask
-import json
-import os
+import flask,json,os,random
 
 CAPTURELINK = os.getenv("CLINK")
 
@@ -8,6 +6,10 @@ app = flask.Flask(__name__)
 
 f = open("./static/projects.json","r")
 plist = json.load(f)
+f.close()
+
+f = open("./static/records.json","r")
+rlist = json.load(f)
 f.close()
 
 @app.route('/')
@@ -21,4 +23,22 @@ def project(pname):
   global plist
   return flask.render_template("project.html",project=plist[pname])
 
+@app.route('/project/portfolio')
+def portfolio():
+  global plist
+  return flask.render_template("portfolio.html",plist=plist)
+
+@app.route('/coolour')
+def randomColour():
+  return flask.render_template_string('''<html style="width:100vw;height:100vh;overflow:hidden;"><head><title>random colour</title></head><body style="background-color:rgb(%s, %s, %s)"></body></html>'''%(random.SystemRandom().randint(0,255),random.SystemRandom().randint(0,255),random.SystemRandom().randint(0,255)))
+
+#DISCORD VERIFY
+@app.route('/.well-known/discord')
+def discord():
+  return "dh=6219b96f5d5f02c814e2f1ed7163995bbe3ff76f"
+
+@app.route('/notes')
+def notes():
+  return flask.render_template("records.html",records=rlist)
+  
 app.run(host='0.0.0.0', port=81)
